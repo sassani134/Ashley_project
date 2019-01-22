1f284ca8e36e555353a92e4ed37b4acce15e8149
sassani134 <sassani134@gmail.com>
2019-01-22 14:57:23 +0100

    active storage undefine methode 'path_for'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
 	
+
 	protect_from_forgery with: :exception
 
 	before_action :configure_devise_parameters, if: :devise_controller?
 
+
+	protected
+
 	def configure_devise_parameters
 		devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation, :avatar) }
+		devise_parameter_sanitizer.permit(:account_update, keys:[:username, :avatar]) 
 	end
 end
